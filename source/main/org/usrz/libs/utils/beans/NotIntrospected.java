f79fde627f6e6c6a47738c1ae14177b7aa1b8f9f
Pier Fumagalli <pier@fumagalli.org>
2014-02-20 01:11:44 +0900

    Import for JavaDOCs

--- source/main/org/usrz/libs/utils/beans/NotIntrospected.java
+++ source/main/org/usrz/libs/utils/beans/NotIntrospected.java
@@ -19,6 +19,7 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
