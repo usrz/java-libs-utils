e76630612cb954cfa48dbeb90647c11a43d6c2dd
Pier Fumagalli <pier@fumagalli.org>
2014-03-30 19:32:33 +0900

    JavaDOC

--- source/main/org/usrz/libs/utils/ObjectPool.java
+++ source/main/org/usrz/libs/utils/ObjectPool.java
@@ -20,31 +20,77 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+/**
+ * An extremely simple <em>object pool</em> constructing objects on
+ * {@linkplain #borrowObject() borrow} and pooling them up to a maximum of
+ * <em>maxSize</em> instances on {@linkplain #returnObject(Object) return}.
+ * <p>
+ * Note that when this pool is empty, it will <em>always</em> call the
+ * configured {@link Supplier} to create new instances.
+ *
+ * @author <a href="mailto:pier@usrz.com">Pier Fumagalli</a>
+ * @param <T> The type of objects stored by this {@link ObjectPool}
+ */
 public class ObjectPool<T> {
 
     private final LinkedBlockingDeque<T> deque;
     private final Supplier<T> supplier;
     private final Predicate<T> predicate;
 
+    /**
+     * Create an {@link ObjectPool} containing up to the specified maxmum
+     * number of instances.
+     * <p>
+     * Objects {@linkplain #returnObject(Object) returned} will be added back
+     * to the pool if <em>not null</em>.
+     *
+     * @param maxSize The maximum number of instances to keep around.
+     * @param supplier A {@link Supplier} to create new instances on borrow.
+     */
     public ObjectPool(int maxSize, Supplier<T> supplier) {
-        this(maxSize, supplier, (instance) -> { return instance != null; });
+        this(maxSize, supplier, notNull());
     }
 
+    /**
+     * Create an {@link ObjectPool} containing up to the specified maxmum
+     * number of instances.
+     * <p>
+     * Objects {@linkplain #returnObject(Object) returned} will be added back
+     * to the pool if <em>not null</em> and the specified {@link Predicate}
+     * returns <em>true</em>.
+     *
+     * @param maxSize The maximum number of instances to keep around.
+     * @param supplier A {@link Supplier} to create new instances on borrow.
+     * @param supplier A {@link Predicate} to check returned instances.
+     */
     public ObjectPool(int maxSize, Supplier<T> supplier, Predicate<T> predicate) {
         if (maxSize < 1) throw new IllegalArgumentException("Invalid size " + maxSize);
         this.supplier = Objects.requireNonNull(supplier, "Null supplier");
-        this.predicate = Objects.requireNonNull(predicate, "Null predicate");
+        this.predicate = ObjectPool.<T>notNull().and(predicate);
         this.deque = new LinkedBlockingDeque<T>(maxSize);
     }
 
+    /**
+     * Borrow an object instance from the pool or create a new instance if one
+     * is not currently available.
+     */
     public T borrowObject() {
         final T instance = deque.pollFirst();
         if (instance != null) return instance;
         return supplier.get();
     }
 
+    /**
+     * Return an object instance to the pool if it does not violate capacity
+     * constraints.
+     */
     public void returnObject(T instance) {
         if (predicate.test(instance)) deque.offerLast(instance);
     }
 
+    /* ====================================================================== */
+
+    private static final <T> Predicate<T> notNull() {
+        return (instance) -> instance != null;
+    }
 }
