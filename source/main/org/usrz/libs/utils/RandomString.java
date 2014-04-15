404ac69eafaf1527e9c51d065bd3459352482cab
Pier Fumagalli <pier@fumagalli.org>
2014-04-15 16:40:02 +0900

    Random strings utility.

---
+++ source/main/org/usrz/libs/utils/RandomString.java
@@ -0,0 +1,39 @@
+/* ========================================================================== *
+ * Copyright 2014 USRZ.com and Pier Paolo Fumagalli                           *
+ * -------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *  http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * ========================================================================== */
+package org.usrz.libs.utils;
+
+import static org.usrz.libs.utils.Check.check;
+
+import java.security.SecureRandom;
+
+/**
+ * Create random strings using a 62-character alphabet {@code [A-Za-z0-9]}.
+ *
+ * @author <a href="mailto:pier@usrz.com">Pier Fumagalli</a>
+ */
+public final class RandomString {
+
+    private static final SecureRandom RANDOM = new SecureRandom();
+    private static final char[] ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+    private static final int BOUND = ALPHABET.length;
+
+    public static String get(int length) {
+        final char[] data = new char[check(length, length > 0, "Invalid length " + length)];
+        for (int x = 0; x < length; x ++) data[x] = ALPHABET[RANDOM.nextInt(BOUND)];
+        return new String(data);
+    }
+
+}
---
+++ source/test/org/usrz/libs/utils/RandomStringTest.java
@@ -0,0 +1,52 @@
+/* ========================================================================== *
+ * Copyright 2014 USRZ.com and Pier Paolo Fumagalli                           *
+ * -------------------------------------------------------------------------- *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *  http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * ========================================================================== */
+package org.usrz.libs.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+import org.usrz.libs.testing.AbstractTest;
+
+public class RandomStringTest extends AbstractTest {
+
+    @Test
+    public void testRandomness() {
+        final Set<String> strings = new HashSet<>();
+        for (int x = 0; x < 1000; x ++) {
+            assertTrue(strings.add(RandomString.get(32)));
+        }
+    }
+
+    @Test
+    public void testLengths() {
+        for (int x = 1; x <= 1000; x ++) {
+            assertEquals(RandomString.get(x).length(), x);
+        }
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class,
+          expectedExceptionsMessageRegExp="Invalid length 0")
+    public void testInvalidLength0() {
+        RandomString.get(0);
+    }
+
+    @Test(expectedExceptions=IllegalArgumentException.class,
+          expectedExceptionsMessageRegExp="Invalid length -1")
+    public void testInvalidLengthNegative() {
+        RandomString.get(-1);
+    }
+}
